05a9a2aac3994262bfc03b6c209bd755f046004b
Class01 <Class01@DESKTOP-UR65MFL>
2023-06-13 18:21:48 +0900

    06.13

---
+++ DateBaseStudy/src/javaOracleQueueCollectionExample/Message.java
@@ -0,0 +1,20 @@
+package javaOracleQueueCollectionExample;
+
+public class Message {
+	public String command;
+	public String to;
+	
+	public Message(String command, String to) {
+		this.command = command;
+		this.to = to;
+		
+	}
+	
+	public String getCommand() {
+		return command;
+	}
+	
+	public String getTo() {
+		return to;
+	}
+}
